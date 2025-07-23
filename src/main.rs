df53cea2eaef8f285cffdf6b05f0ec201b4579fb
Kevin Donnelly <kevin.donnelly@outlook.de>
2025-07-23 23:05:45 +0200

    Add player and player movement

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use rltk::{GameState, RGB};
+use std::cmp::{min, max};
+use rltk::{GameState, Rltk, VirtualKeyCode, RGB};
 use specs::prelude::*;
 use specs_derive::Component;
 
@@ -34,6 +35,7 @@ impl GameState for State {
     fn tick(&mut self, ctx: &mut rltk::Rltk) {
         ctx.cls();
 
+        player_input(self, ctx);
         self.run_systems();
 
         let positions = self.ecs.read_storage::<Position>();
@@ -58,10 +60,37 @@ impl<'a> System<'a> for LeftWalker {
     }
 }
 
+#[derive(Component, Debug)]
+struct Player {}
+
+fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
+    let mut positions = ecs.write_storage::<Position>();
+    let mut players = ecs.write_storage::<Player>();
+
+    for (_player, pos) in (&mut players, &mut positions).join() {
+        pos.x = min(79, max(0, pos.x + delta_x));
+        pos.y = min(49, max(0, pos.y + delta_y));
+    }
+}
+
+fn player_input(gs: &mut State, ctx: &mut Rltk) {
+    match ctx.key {
+        None => {}
+        Some(key) => match key {
+            VirtualKeyCode::Left => try_move_player(-1, 0, &mut gs.ecs),
+            VirtualKeyCode::Right => try_move_player(1, 0, &mut gs.ecs),
+            VirtualKeyCode::Up => try_move_player(0, -1, &mut gs.ecs),
+            VirtualKeyCode::Down => try_move_player(0, 1, &mut gs.ecs),
+            _ => {}
+        }
+    }
+}
+
 fn register_structs(ecs: &mut World) {
     ecs.register::<Position>();
     ecs.register::<Renderable>();
     ecs.register::<LeftMover>();
+    ecs.register::<Player>();
 }
 
 fn create_entities(ecs: &mut World) {
@@ -74,7 +103,9 @@ fn create_entities(ecs: &mut World) {
             glyph: rltk::to_cp437('@'),
             fg: RGB::named(rltk::YELLOW),
             bg: RGB::named(rltk::BLACK),
-        }).build();
+        })
+        .with(Player {})
+        .build();
 
     for i in 0..10 {
         ecs.create_entity()

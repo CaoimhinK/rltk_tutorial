4b741ef580269c96d67ad351a0a00c6586593117
Kevin Donnelly <kevin.donnelly@outlook.de>
2025-07-24 23:09:49 +0200

    Update visibility system

--- src/visibility_system.rs
+++ src/visibility_system.rs
@@ -1,15 +1,21 @@
+use rltk::{field_of_view, Point};
 use specs::prelude::*;
-use crate::{Viewshed, Position};
+use crate::{Map, Position, Viewshed};
 
 pub struct VisibilitySystem {}
 
 impl<'a> System<'a> for VisibilitySystem {
-    type SystemData = ( WriteStorage<'a, Viewshed>,
+    type SystemData = ( ReadExpect<'a, Map>,
+                        WriteStorage<'a, Viewshed>,
                         WriteStorage<'a, Position>);
     
-    fn run(&mut self, (mut viewshed, pos): Self::SystemData) {
+    fn run(&mut self, data: Self::SystemData) {
+        let (map, mut viewshed, pos) = data;
+        
         for (viewshed, pos) in (&mut viewshed, &pos).join() {
-            
+            viewshed.visible_tiles.clear();
+            viewshed.visible_tiles = field_of_view(Point::new(pos.x, pos.y), viewshed.range, &*map);
+            viewshed.visible_tiles.retain(|p| p.x > 0 && p.x < map.width && p.y >= 0 && p.y < map.height);
         }
     }
 }

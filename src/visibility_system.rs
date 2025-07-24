48c44441954f729eca920141420a0c7f96b7e5ce
Kevin Donnelly <kevin.donnelly@outlook.de>
2025-07-24 23:35:08 +0200

    Remember viewed map

--- src/visibility_system.rs
+++ src/visibility_system.rs
@@ -1,21 +1,31 @@
 use rltk::{field_of_view, Point};
 use specs::prelude::*;
-use crate::{Map, Position, Viewshed};
+use crate::{Map, Player, Position, Viewshed};
 
 pub struct VisibilitySystem {}
 
 impl<'a> System<'a> for VisibilitySystem {
-    type SystemData = ( ReadExpect<'a, Map>,
+    type SystemData = ( WriteExpect<'a, Map>,
+                        Entities<'a>,
                         WriteStorage<'a, Viewshed>,
-                        WriteStorage<'a, Position>);
+                        WriteStorage<'a, Position>,
+                        ReadStorage<'a, Player>);
     
     fn run(&mut self, data: Self::SystemData) {
-        let (map, mut viewshed, pos) = data;
+        let (mut map, entities, mut viewshed, pos, player) = data;
         
-        for (viewshed, pos) in (&mut viewshed, &pos).join() {
+        for (ent, viewshed, pos) in (&entities, &mut viewshed, &pos).join() {
             viewshed.visible_tiles.clear();
             viewshed.visible_tiles = field_of_view(Point::new(pos.x, pos.y), viewshed.range, &*map);
             viewshed.visible_tiles.retain(|p| p.x > 0 && p.x < map.width && p.y >= 0 && p.y < map.height);
+
+            let p: Option<&Player> = player.get(ent);
+            if let Some(p) = p {
+                for vis in viewshed.visible_tiles.iter() {
+                    let idx = map.xy_idx(vis.x, vis.y);
+                    map.revealed_tiles[idx] = true;
+                }
+            }
         }
     }
 }
